4f84ad05d0228e36272bde1615ab5abbd5b7215d
saza <saza_thi@hotmail.com>
2016-02-23 12:14:11 +0700

    newlatter

--- src/newsaza/tests.py
+++ src/newsaza/tests.py
@@ -1,3 +1,4 @@
-from django.test import TestCase
+from django.test import TestCas
+
 
 # Create your tests here.
--- src/trydjango18/settings.py
+++ src/trydjango18/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'newsaza',
+    'newslatter',
 )
 
 MIDDLEWARE_CLASSES = (
