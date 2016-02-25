a049822c05deb64bc34228aaf912a64b5aaaca95
zsrichock <saza_thi@hotmail.com>
2016-02-25 22:58:12 +0700

    Merge branch 'master' of https://github.com/sznote/myapp01
    # Conflicts:
    #	src/newsaza/templates/home.html

--- src/newsaza/views.py
+++ src/newsaza/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+#from django.forms import forms
+#from .models import SignUP
 
 # Create your views here.
 
 def home(request):
-    return render(request,"home.html",{})
+    title = "My Title"
+    mytitle = {
+        "template_title": "hello",
+        "template_user": "username",
+        "template_sur": "surname",
+    }
+    context = {
+        "template_user": "sahai",
+        "template_sur": "srichock",
+    }
+
+    #return render(request,"home.html",{})
+    return render(request,"home.html",context=mytitle)
