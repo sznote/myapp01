855cbd6483f6bd4a065db0da53da1a7cb8941990
zsrichock <saza_thi@hotmail.com>
2016-03-18 21:49:20 +0700

    Merge branch 'master' of https://github.com/sznote/myapp01
    # Conflicts:
    #	src/newsaza/templates/home.html

--- src/trydjango18/settings.py
+++ src/trydjango18/settings.py
@@ -13,8 +13,9 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-BASE_DIR = os.path.dirname((os.path.abspath(__file__)))
-#BASE_DIR = os.path.dirname(os.path.dirname((os.path.abspath(__file__))))
+#BASE_DIR = os.path.dirname((os.path.abspath(__file__)))
+
+BASE_DIR = os.path.dirname(os.path.dirname((os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -85,6 +86,7 @@ WSGI_APPLICATION = 'trydjango18.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(os.path.dirname(BASE_DIR), 'db.sqlite3'),
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
